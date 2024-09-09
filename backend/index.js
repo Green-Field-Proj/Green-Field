957e5afbd44d337c41eb7555a7ee042a74df94fe
............... .......... <Iheb>
2024-09-09 11:19:01 +0100

    added cookie parser middleware

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const userRouter = require("./routers/UserRouter");
 require("dotenv").config();
 require("./config");
@@ -13,7 +14,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(cookieParser()); // middleware to parse cookies where our token and role is stored
 // Routes
 app.use("/api", Router);
 app.use("/api/user", userRouter);
