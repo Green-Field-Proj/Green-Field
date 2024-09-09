7d3e6b524460b27a656fd1ff01fb91c280c73a63
Hamza <hamzafougahli@gmail.com>
2024-09-09 10:44:32 +0100

    Login/signUp, done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,106 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import "../style/App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGoogle,
+  faFacebookF,
+  faYoutube,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
-function App() {
+const App = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div>
-      <h1>Welcome to Green Field project</h1>
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form action="#">
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+          <span>or use your email for register</span>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in">
+        <form action="#">
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+            <a href="#" className="icon">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+          <span>or use your email and password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot Password?</a>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              Enter your personal details to use all of the site's features.
+            </p>
+            <button className="hidden" id="login" onClick={handleLoginClick}>
+              Sign In
+            </button>
+          </div>
+
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Subscriber!</h1>
+            <p>
+              Register with your personal details to use all of the site's
+              features.
+            </p>
+            <button
+              className="hidden"
+              id="register"
+              onClick={handleRegisterClick}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
