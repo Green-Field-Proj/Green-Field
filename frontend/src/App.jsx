fce1b723b143c91df78b9580b5de7379a0ea2fbd
Hamza <149156223+Hamza-Foughali@users.noreply.github.com>
2024-09-09 12:12:08 +0100

    Login/signUp, done (#3)
    * Login/signUp, done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import "../style/App.css";
+import SignUpForm from "../src/Login&SignUp/loginform.jsx";
+import SignInForm from "../src/Login&SignUp/signupform.jsx";
+import TogglePanel from "../src/Login&SignUp/Toggle.jsx";
 
-function App() {
+const App = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div>
-      <h1>Welcome to Green Field project</h1>
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <SignUpForm />
+      <SignInForm />
+      <TogglePanel
+        onRegisterClick={handleRegisterClick}
+        onLoginClick={handleLoginClick}
+      />
     </div>
   );
-}
+};
 
 export default App;
