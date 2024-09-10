de7f90846288a1cfd104ee69ddf0960a110bd1ba
............... .......... <Iheb>
2024-09-10 22:42:42 +0100

    latest version

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { TextField, Button } from "@mui/material";
-import signuplogin from "../images/signuplogin.png"
-
+import signuplogin from "../images/signuplogin.png";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -29,7 +28,7 @@ function Login() {
         navigate("/");
       }, 2000);
     } catch (error) {
-      setErrorMessage("Login failed. Please try again.");
+      setErrorMessage(error.response.data.message);
       setSuccessMessage("");
     }
   };
@@ -37,15 +36,15 @@ function Login() {
   return (
     <div className="full-login">
       <div className="image-container">
-    <img src={signuplogin} alt="" />
-    </div>
-    <div className="login-form">
-      <h2>Log in to Exclusive</h2>
-      <h3>Enter your details below</h3>
-      {successMessage && <p className="success-message">{successMessage}</p>}
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
-      <form onSubmit={handleSubmit}>
-        {/* <div>
+        <img src={signuplogin} alt="" />
+      </div>
+      <div className="login-form">
+        <h2>Log in to Exclusive</h2>
+        <h3>Enter your details below</h3>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          {/* <div>
           <label htmlFor="email">Email:</label>
           <input
             type="email"
@@ -66,33 +65,33 @@ function Login() {
           />
         </div> */}
 
-<div>
-          <TextField
-            id="email"
-            name="email" 
-            label="Email"
-            variant="standard"
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <TextField
-            id="password"
-            name="password" 
-            label="Password"
-            type="password" 
-            variant="standard"
-            fullWidth
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Log In</button>
-        <p id="forgotPass">Forget Password?</p>
-      </form>
-    </div>
+          <div>
+            <TextField
+              id="email"
+              name="email"
+              label="Email"
+              variant="standard"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              variant="standard"
+              fullWidth
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Log In</button>
+          <p id="forgotPass">Forget Password?</p>
+        </form>
+      </div>
     </div>
   );
 }
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import TextField from '@mui/material/TextField';
-import { Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import signuplogin from "../images/signuplogin.png"
-import '@fontsource/roboto/400.css'; 
-import '@fontsource/roboto/700.css'; 
-
+import TextField from "@mui/material/TextField";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import signuplogin from "../images/signuplogin.png";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 
 function SignUp() {
   const [formData, setFormData] = useState({
@@ -41,61 +46,63 @@ function SignUp() {
         navigate("/login");
       }, 2000);
     } catch (error) {
-      setErrorMessage("Sign up failed. Please try again.");
+      setErrorMessage(error.response.data.message);
       setSuccessMessage("");
     }
   };
 
   return (
     <div className="full-signup">
-    <div className="image-container">
-    <img src={signuplogin} alt="" />
-    </div>
+      <div className="image-container">
+        <img src={signuplogin} alt="" />
+      </div>
 
-    <div className="signup-container">
-      <h2>Create an account</h2>
-      <h3>Enter your details below</h3>
-      {successMessage && (
-        <p className="message success-message">{successMessage}</p>
-      )}
-      {errorMessage && <p className="message error-message">{errorMessage}</p>}
-      <form onSubmit={handleSubmit}>
-      <div>
-          <TextField
-            id="username"
-            name="username" 
-            label="Name"
-            variant="standard"
-            fullWidth
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <TextField
-            id="email"
-            name="email" 
-            label="Email"
-            variant="standard"
-            fullWidth
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <TextField
-            id="password"
-            name="password" 
-            label="Password"
-            type="password" 
-            variant="standard"
-            fullWidth
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </div>
-         <div id="roleDiv">
-          {/* <select
+      <div className="signup-container">
+        <h2>Create an account</h2>
+        <h3>Enter your details below</h3>
+        {successMessage && (
+          <p className="message success-message">{successMessage}</p>
+        )}
+        {errorMessage && (
+          <p className="message error-message">{errorMessage}</p>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <TextField
+              id="username"
+              name="username"
+              label="Name"
+              variant="standard"
+              fullWidth
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <TextField
+              id="email"
+              name="email"
+              label="Email"
+              variant="standard"
+              fullWidth
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              variant="standard"
+              fullWidth
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div id="roleDiv">
+            {/* <select
             id="role"
             name="role"
             value={formData.role}
@@ -106,36 +113,49 @@ function SignUp() {
             <option value="seller">Seller</option>
           </select> */}
 
-<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="roleLabel">Role</InputLabel>
-      <Select
-        labelId="role"
-        name="role"
-        id="role"
-        value={formData.role}
-        label="role"
-        onChange={handleChange}
-      >
-        <MenuItem value='client'>Client</MenuItem>
-        <MenuItem value='seller'>Seller</MenuItem>
-          </Select>
-    </FormControl>
-        </div>
-        <div>
-          <label htmlFor="imageUrl">Profile Image URL:</label>
-          <input
-            type="url"
-            id="imageUrl"
-            name="imageUrl"
-            value={formData.imageUrl}
-            onChange={handleChange}
-          />
-        </div> 
-       <Button variant="contained" type="submit" disableElevation>Create Account</Button>
-       
-      </form><br/>
-      <p className="haveAccount">Already have account?<p className="haveAccount" id="haveAccountLogIn" onClick={()=>{navigate("/login")}}>Log in</p></p>
-    </div>
+            <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+              <InputLabel id="roleLabel">Role</InputLabel>
+              <Select
+                labelId="role"
+                name="role"
+                id="role"
+                value={formData.role}
+                label="role"
+                onChange={handleChange}
+              >
+                <MenuItem value="client">Client</MenuItem>
+                <MenuItem value="seller">Seller</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div>
+            <label htmlFor="imageUrl">Profile Image URL:</label>
+            <input
+              type="url"
+              id="imageUrl"
+              name="imageUrl"
+              value={formData.imageUrl}
+              onChange={handleChange}
+            />
+          </div>
+          <Button variant="contained" type="submit" disableElevation>
+            Create Account
+          </Button>
+        </form>
+        <br />
+        <p className="haveAccount">
+          Already have account?
+          <span
+            className="haveAccount"
+            id="haveAccountLogIn"
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            Log in
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
