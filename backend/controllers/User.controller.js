a6817d5af05ea8dabab582145fd6512fd725f8c5
iheb jendoubi <75304975+kuroxdev@users.noreply.github.com>
2024-09-12 23:01:11 +0100

    Iheb (#23)
    * latest version
    
    * changes to cors
    
    * fixed db query added boileplate for our home,added redux & reduxtoolkit for user auth
    
    * simple sale countdown added admin/seller middleware for future implementation
    
    * secure routes in the frontend & fixed some security issues
    
    * minor bug fixes
    
    ---------
    
    Co-authored-by: ............... .......... <Iheb>

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -15,7 +15,7 @@ module.exports = {
     try {
       const { username, email, password, role, profileImage } = req.body;
       // Input validation
-      if (role !== "user" && role !== "client") {
+      if (role !== "seller" && role !== "client") {
         return res.status(400).json({ message: "Invalid role" });
       }
       if (!username || !email || !password || !role) {
@@ -70,6 +70,13 @@ module.exports = {
         { expiresIn: "1h" }
       );
       res.cookie("token", token, { httpOnly: true });
+      res.status(200).json({
+        message: "Login successful",
+        role: user.role,
+        userName: user.username,
+        email: user.email,
+        profilePicture: user.profileImage,
+      });
       res.status(200).json({
         message: "Login successful",
         role: user.role,
@@ -178,6 +185,24 @@ module.exports = {
       }
     } catch (error) {
       res.status(500).send("Internal server error");
+    }
+  },
+  checkStatus: async (req, res) => {
+    const id = req.user.id;
+
+    const user = await User.findByPk(id, {
+      attributes: ["username", "email", "profileImage", "role"],
+    });
+
+    res.status(200).send(user);
+  },
+  userLogout: async (req, res) => {
+    try {
+      res.clearCookie("token", { httpOnly: true });
+      res.status(200).json({ message: "Logged out successfully" });
+      console.log("Logged out successfully");
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
   },
   checkStatus: async (req, res) => {
